16c0aee57076563ea4988554c9d5e63907d5826c
Sumit Pal <mr.pal92@gmail.com>
2018-10-08 18:17:17 +0530

    Adding the project to define exceptions.

---
+++ Assessment.Exception/CustomException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment.Exception
+{
+    public class CustomException
+    {
+        private string errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { errorMessage = value; }
+        }
+
+        public CustomException(string errorMessage ="Some Error Occurred")
+        {
+            ErrorMessage = errorMessage;
+        }
+    }
+}
